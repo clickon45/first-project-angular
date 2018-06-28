5ceaafb990ecfd8dc15ad38f42c9362f98ac8969
clickon45 <nandkishor.j.shinde@gmail.com>
2018-06-28 17:22:53 +0530

    Header Containt Change

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.module';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Chef John`s Pasta Primavera', 'Pasta primavera is quite a straightforward recipe. ', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Recipe('Chef Roshan`Pasta', 'Pasta Masala is quite a straightforward recipe. ', 'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png')
+  ];
 
   constructor() { }
 
